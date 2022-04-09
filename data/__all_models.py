648f73377efbf5724fa1fee847e9a758c9716ed6
PlayerUCW <783426Zzz@gmail.com>
2022-04-09 16:23:25 +0300

    add everything

---
+++ data/__all_models.py
@@ -0,0 +1,53 @@
+from .db_session import SqlAlchemyBase
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+import sqlalchemy
+from sqlalchemy import orm
+import datetime
+
+
+class User(SqlAlchemyBase, UserMixin):
+    __tablename__ = 'users'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    surname = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    name = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    age = sqlalchemy.Column(sqlalchemy.Integer, nullable=True)
+    sex = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    position = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    speciality = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    address = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
+    hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    modified_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
+    department = orm.relation("Department")
+
+    def set_password(self, password):
+        self.hashed_password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.hashed_password, password)
+
+
+class Jobs(SqlAlchemyBase):
+    __tablename__ = 'jobs'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    team_leader = sqlalchemy.Column(sqlalchemy.Integer)
+    job = sqlalchemy.Column(sqlalchemy.String)
+    work_size = sqlalchemy.Column(sqlalchemy.Integer)
+    collaborators = sqlalchemy.Column(sqlalchemy.String)
+    start_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    end_date = sqlalchemy.Column(sqlalchemy.DateTime, nullable=True)
+    is_finished = sqlalchemy.Column(sqlalchemy.Boolean)
+
+
+class Department(SqlAlchemyBase):
+    __tablename__ = 'departments'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String)
+    chief = sqlalchemy.Column(sqlalchemy.Integer)
+    email = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    user = orm.relation("User", back_populates="departments")
